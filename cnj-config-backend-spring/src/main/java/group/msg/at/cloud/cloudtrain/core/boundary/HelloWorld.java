fd61d475eea5952215e3302a460f6b2cc88df1b4
Michael Theis <michael.theis@msg.group>
2019-06-25 10:41:42 +0200

    released version 1.0.0
    - completed Spring backend as well.

--- cnj-config-backend-spring/src/main/java/group/msg/at/cloud/cloudtrain/core/boundary/HelloWorld.java
+++ cnj-config-backend-spring/src/main/java/group/msg/at/cloud/cloudtrain/core/boundary/HelloWorld.java
@@ -1,6 +1,7 @@
 package group.msg.at.cloud.cloudtrain.core.boundary;
 
 import group.msg.at.cloud.cloudtrain.core.entity.Message;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Locale;
@@ -9,10 +10,16 @@ import java.util.UUID;
 @Service
 public class HelloWorld {
 
+    @Value("${cloudtrain.config.stringValue}")
+    private String stringConfigValue;
+
+    @Value("${cloudtrain.config.numericValue}")
+    private int numericConfigValue;
+
     public Message getHelloMessage() {
         Message result = new Message(UUID.randomUUID());
         result.setCode("hello");
-        result.setText("Welcome to Cloud Native Java with Spring Boot!");
+        result.setText(String.format("Welcome to Cloud Native Java with %s! numericConfigValue : %d", this.stringConfigValue, this.numericConfigValue));
         result.setLocale(Locale.GERMAN);
         return result;
     }
